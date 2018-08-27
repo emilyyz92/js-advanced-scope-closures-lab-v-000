abdb6fc2f88c9edd98a331f1b1f4ed55b1852a3d
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-27 20:28:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+const produceDrivingRange = function(blockRange) {
+  return function (begRange, endRange) {
+    let beg = parseInt(begRange.slice(0, 2))
+    let end = parseInt(endRange.slice(0, 2))
+    const distance = end - beg - blockRange
+    if (distance > 0) {
+      return `${distance} blocks out of range`
+    } else {
+      return `within range by ${-distance}`
+    }
+  }
+}
+
+const produceTipCalculator = function(percent) {
+  return function(amount) {
+    return amount * percent
+  }
+}
+
+const createDriver = function() {
+  let driverId = 0;
+  return class Driver {
+    constructor (name) {
+      this.name = name
+      this.id = driverId++
+    }
+  }
+}
