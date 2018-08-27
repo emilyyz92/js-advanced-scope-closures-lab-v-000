28959d916ac1b946770ab239dcc2efa0ac1cfcbb
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-27 20:13:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,6 @@
+const produceDrivingRange = function(blockRange) {
+  return function (begRange, endRange) {
+    let begRange = begRange.slice(0, 2)
+    let endRange = endRange.slice(0, 2)
+  }
+}
