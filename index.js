5cc5e09b7e7b1976eb3285e02bd3cc97a9ae4eb3
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-27 20:19:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const produceDrivingRange = function(blockRange) {
   return function (begRange, endRange) {
     let begRange = parseInt(begRange.slice(0, 2))
     let endRange = parseInt(endRange.slice(0, 2))
-
+    const distance = endRange - begRange - blockRange
+    if (distance > 0) {
+      return `${distance} blocks out of range`
+    } else {
+      return `within range by ${distance}`
+    }
   }
 }
